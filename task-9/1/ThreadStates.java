4af27a328044c642223ccedaf0fb8e46535810cb
Харьковец Агния <agnia.kharkovets@yandex.ru>
2025-12-21 03:20:38 +0300

    дз по многозадачности

---
+++ task-9/1/ThreadStates.java
@@ -0,0 +1,46 @@
+public class ThreadStates {
+    public static void main(String[] args) throws Exception {
+        Object lock = new Object();
+        
+        Thread thread = new Thread(() -> {
+            synchronized(lock) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {}
+            }
+            
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {}
+            
+            synchronized(ThreadStates.class) {
+                System.out.println("Завершаем работу");
+            }
+        });
+        
+        // Захватываем монитор класса для состояния BLOCKED
+        synchronized(ThreadStates.class) {
+            System.out.println("NEW: " + thread.getState());
+            
+            thread.start();
+            Thread.sleep(50);
+            
+            System.out.println("RUNNABLE (кратко): " + thread.getState());
+            
+            // Будим поток
+            synchronized(lock) {
+                lock.notify();
+            }
+            
+            Thread.sleep(50);
+            System.out.println("WAITING завершилось, сейчас TIMED_WAITING: " + thread.getState());
+            
+            Thread.sleep(1100);
+            System.out.println("BLOCKED: " + thread.getState());
+        }
+        
+        Thread.sleep(50);
+        thread.join();
+        System.out.println("TERMINATED: " + thread.getState());
+    }
+}
