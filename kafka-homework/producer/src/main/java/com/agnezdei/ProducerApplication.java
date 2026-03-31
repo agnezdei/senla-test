c13efb2a4bb6717fbadbbea74bba4eb92a629780
agnezadei <agnia.kharkovets@yandex.ru>
2026-03-31 11:14:14 +0300

    уникальный бин

--- kafka-homework/producer/src/main/java/com/agnezdei/ProducerApplication.java
+++ kafka-homework/producer/src/main/java/com/agnezdei/ProducerApplication.java
@@ -4,8 +4,11 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.transaction.KafkaTransactionManager;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.TransactionManager;
 
 @SpringBootApplication
 @EnableScheduling
@@ -21,5 +24,11 @@ public class ProducerApplication {
                 .partitions(3)
                 .replicas(3)
                 .build();
+    }
+
+    @Primary
+    @Bean
+    public TransactionManager transactionManager(KafkaTransactionManager<?, ?> kafkaTm) {
+        return kafkaTm;
     }
 }
